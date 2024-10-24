f41edf0902072705e37beb17394f856e6915c427
Leonardo Christ <christ.leonardo21@gmail.com>
2024-10-24 13:26:19 -0300

    corrido alguns bugs

--- blog/models/post.py
+++ blog/models/post.py
@@ -12,15 +12,11 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     updated = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
-        ordering = ['-created']
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
-
-
-def post():
-    return None
--- iniciando/tests/models/test_post.py
+++ iniciando/tests/models/test_post.py
@@ -4,8 +4,8 @@ from iniciando.factories import PostFactory
 
 @pytest.fixture
 def post_published():
-    return PostFactory(title='Post published')
+    return PostFactory(title='pytest with factory')
 
 @pytest.mark.django_db
 def test_create_published_post(post_published):
-    assert post_published.title == 'Post published'
+    assert post_published.title == 'pytest with factory'
