8e56e4cefa4be6b62d9bbc888b3f61d3057c58d0
Leonardo Christ <christ.leonardo21@gmail.com>
2024-10-25 16:51:40 -0300

    integrando as paginas HTML

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PostView
+
+from blog.views import post_view
 
 urlpatterns = [
-    path('post_view/', PostView.as_view(), name='post_view'),
+    path('post_view/', post_view.PostView.as_view(), name='post_view'),
+    path("<slug:slug>/", post_view.PostDetail.as_view(), name='post_detail'),
 ]
--- iniciando/urls.py
+++ iniciando/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from blog.views.post_view import PostView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('blog.urls')),
+    path('home/', PostView.as_view(), name='home'),
+    path('blog/', include('blog.urls')),
 ]
